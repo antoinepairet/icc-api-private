cca1ec476f31f3f4ec7d23ede1fa715d1ab1d956
Cédric Bamps <cedric.bamps@b12-consulting.com>
2018-05-31 16:27:26 +0200

    modify crypto.randomUuid for typescript compatibility

--- icc-x-api/icc-crypto-x-api.ts
+++ icc-x-api/icc-crypto-x-api.ts
@@ -23,7 +23,7 @@ export class IccCryptoXApi {
 	}
 
 	randomUuid() {
-		return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ window.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+		return (1e7.toString() + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (Number(c) ^ window.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> Number(c) / 4).toString(16));
 	}
 
 	decryptHcPartyKey(delegatorId, delegateHcPartyId, encryptedHcPartyKey, encryptedForDelegator) {
